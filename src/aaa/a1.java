39b412371c1db3ea74d41fc05551ddf02759da27
Krishna Kanani <kananikrishna427@gmail.com>
2024-02-28 13:38:22 +0530

    First commit

---
+++ src/aaa/a1.java
@@ -0,0 +1,9 @@
+package aaa;
+
+public class a1 {
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ src/bbb/b1.java
@@ -0,0 +1,9 @@
+package bbb;
+
+public class b1 {
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
